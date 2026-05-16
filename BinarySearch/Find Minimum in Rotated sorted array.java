7687fcdf381cd4c8891fc25f15888139b652b415
RAJMAN <112617407+Rajman1221@users.noreply.github.com>
2026-05-16 16:42:23 +0530

    Update Find Minimum in Rotated sorted array.java

--- BinarySearch/Find Minimum in Rotated sorted array.java
+++ BinarySearch/Find Minimum in Rotated sorted array.java
@@ -1,6 +1,6 @@
 //this is problem where we are given with an rotated sorted array where we have to find minimum element in O(logn)..
 //so we gonna use Binary search method as the array is sorted.
-
+//there must be a point in the array from where its rotated, that means we have two separated sorted half left half and right half
 class Solution {
     public int findMin(int[] nums) {
         int n=nums.length;
@@ -8,6 +8,8 @@ class Solution {
         int r=n-1;
         while(l<r){
             int mid=l+(r-l)/2;
+            //here we check that the left half is sorted or not (matlb agar nums[mid]>nums[r] hai toh mid ke baad saare chote element hai )right side of the mid 
+            //we will get our ans..
             if(nums[mid]>nums[r]){
                 l=mid+1;
             }else{
